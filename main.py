4589bdbfa4aacb776965f7fdb2cae18922ea037e
wi1lku <piotrwil288@gmail.com>
2021-08-20 21:29:26 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+const int rep = 100000; #repetitions
+
+
+ans = [0 for i in range(10)]
+
+i = 1
+
+while(i<=rep):
+	try:
+		html_text = requests.get('https://en.wikipedia.org/wiki/Special:Random').text
+		soup = BeautifulSoup(html_text, 'lxml')
+
+		print(f'{i}. checking website {soup.title.text}')
+
+		content = soup.find('div', class_ = "mw-parser-output")
+
+		numbers = re.findall('[1-9][0-9]+', content.text)
+
+		for number in numbers:
+			x = int(number[0])
+			ans[x] = ans[x] + 1
+		i+=1
+	except:
+		print("Error!")
+
+print(ans)
